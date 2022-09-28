af5f4339f39dd23dccbb5e34d698d2e0577a9e06
ZiuChen <457353192@qq.com>
2022-09-28 11:28:20 +0800

    modify 9(ts) 2022/09/28

--- 9.回文数.ts
+++ 9.回文数.ts
@@ -5,13 +5,23 @@
  */
 
 // @lc code=start
+// 前一半后一半数字对比
+// function isPalindrome(x: number): boolean {
+//   if (x < 0 || (x % 10 === 0 && x !== 0)) return false
+//   let revNumber = 0
+//   while (x > revNumber) {
+//     revNumber = revNumber * 10 + (x % 10)
+//     x = Math.floor(x / 10)
+//   }
+//   return x === revNumber || x === Math.floor(revNumber / 10)
+// }
+// 转字符串
 function isPalindrome(x: number): boolean {
-  if (x < 0 || (x % 10 === 0 && x !== 0)) return false
-  let revNumber = 0
-  while (x > revNumber) {
-    revNumber = revNumber * 10 + (x % 10)
-    x = Math.floor(x / 10)
+  const string = String(x)
+  let r = ''
+  for (let i = string.length - 1; i >= 0; i--) {
+    r += string[i]
   }
-  return x === revNumber || x === Math.floor(revNumber / 10)
+  return r === string
 }
 // @lc code=end
