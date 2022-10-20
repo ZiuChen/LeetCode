b896ea78853012d8708de051686d351e97fe72e7
ZiuChen <457353192@qq.com>
2022-10-20 16:37:15 +0800

    update 34(ts) 2022/10/20

--- 34.在排序数组中查找元素的第一个和最后一个位置.ts
+++ 34.在排序数组中查找元素的第一个和最后一个位置.ts
@@ -5,30 +5,43 @@
  */
 
 // @lc code=start
-function searchRange(nums: number[], target: number): number[] {
-  // 此解不符合题意: 时间复杂度O(n) 没有利用到数组升序排列的条件
+// lower==true:  第一个>=target的下标
+// lower==false: 第一个>target的下标
+const binarySearch = (nums: number[], target: number, lower: boolean) => {
   let left = 0
   let right = nums.length - 1
+  let ans = nums.length
+  // 二分法找target附近下标
   while (left <= right) {
-    // 左右边界都与target相等 则左右边界已确定
-    if (nums[left] === target && nums[right] === target) {
-      return [left, right]
-    }
-    // nums[left]比target小
-    // 左边界不符合条件 向右移动
-    if (nums[left] < target) {
-      left++
-    }
-    // nums[right]比target大
-    // 右边界不符合条件 向左移动
-    if (nums[right] > target) {
-      right--
+    const mid = Math.floor((right + left) / 2)
+    // lower==true时 找>=的 否则找>的
+    if (nums[mid] > target || (lower && nums[mid] >= target)) {
+      // target在左半边 右指针左移
+      right = mid - 1
+      ans = mid
+    } else {
+      // target在右半边 左指针右移
+      left = mid + 1
     }
   }
-  return [-1, -1]
+  return ans
+}
+function searchRange(nums: number[], target: number): number[] {
+  // 利用数组升序排列的条件 二分法来加速查找
+  // 找到: 1.第一个等于target的位置 & 2.第一个大于target的位置-1
+  let ans = [-1, -1]
+  const leftIndex = binarySearch(nums, target, true) // 找target下标
+  const rightIndex = binarySearch(nums, target, false) - 1 // 找大于target的下标 然后-1
+  // target可能不存在于数组中: 检查左右下标是否合法
+  if (
+    leftIndex <= rightIndex &&
+    rightIndex < nums.length &&
+    nums[leftIndex] === target &&
+    nums[rightIndex] === target
+  ) {
+    // 合法则正常返回 否则返回[-1, -1]
+    ans = [leftIndex, rightIndex]
+  }
+  return ans
 }
 // @lc code=end
-
-console.log(searchRange([5, 7, 7, 8, 8, 10], 8))
-console.log(searchRange([5, 7, 7, 8, 8, 10], 6))
-console.log(searchRange([], 0))
