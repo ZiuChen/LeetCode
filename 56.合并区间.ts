fded60ea2d5fbcce24b76b273d7bce9e089d526d
ZiuChen <457353192@qq.com>
2022-10-27 21:48:39 +0800

    update 56(ts) 2022/10/27

--- 56.合并区间.ts
+++ 56.合并区间.ts
@@ -8,25 +8,26 @@
 function merge(intervals: number[][]): number[][] {
   if (intervals.length <= 1) return intervals
 
-  let i = 0 // 指向当前正在处理的区间
-
   // 按左端点值排序 保证可合并区间是连续的
   intervals.sort((a, b) => a[0] - b[0])
 
-  while (intervals[i + 1]) {
-    // 如果i与i+1区间有同值或交叉 则合并两区间
-    if (intervals[i][1] >= intervals[i + 1][0]) {
-      // 更新i+1的左右区间值 左区间取最小 右区间取最大
-      intervals[i + 1][0] = Math.min(intervals[i][0], intervals[i + 1][0])
-      intervals[i + 1][1] = Math.max(intervals[i][1], intervals[i + 1][1])
+  const res: number[][] = [intervals[0]] // 直接将排序后的第一个区间加入结果
 
-      intervals.splice(i, 1) // 移除i
-      i-- // 移除后相当于整体前移了
+  for (let i = 1; i < intervals.length; i++) {
+    const end = res[res.length - 1]
+    // 符合合并条件 修改最末区间执行合并
+    // 当前区间左端点 在最末区间范围内
+    if (intervals[i][0] >= end[0] && intervals[i][0] <= end[1]) {
+      // 取完整区间: 左端点取最小的 右端点取最大的
+      const t = [Math.min(end[0], intervals[i][0]), Math.max(end[1], intervals[i][1])]
+      res[res.length - 1] = t
+      continue // 跳出此次循环
     }
-    i++
+    // 不能合并 直接添加
+    res.push(intervals[i])
   }
 
-  return intervals
+  return res
 }
 // @lc code=end
 
