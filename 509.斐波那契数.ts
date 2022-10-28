412434db321e78266b421f5199a3084ce4b5dd3a
ZiuChen <457353192@qq.com>
2022-10-28 09:43:31 +0800

    update 509(ts) 2022/10/28

--- 509.斐波那契数.ts
+++ 509.斐波那契数.ts
@@ -6,20 +6,17 @@
 
 // @lc code=start
 function fib(n: number): number {
-  // 记忆已经求结果的n值
-  const memo: { [key: number]: number } = {}
+  if (n <= 1) return n
 
-  const _res = (x: number): number => {
-    // 找到了已求解过的值 直接返回
-    if (memo[x]) return memo[x]
-    if (x === 0) return 0
-    if (x === 1) return 1
+  const dp: number[] = [0, 1]
 
-    memo[x] = _res(x - 1) + _res(x - 2)
-    return memo[x]
+  for (let i = 2; i <= n; i++) {
+    const sum = dp[0] + dp[1]
+    dp[0] = dp[1]
+    dp[1] = sum
   }
 
-  return _res(n)
+  return dp[1]
 }
 // @lc code=end
 
