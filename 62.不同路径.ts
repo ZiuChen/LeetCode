f745afb9621a64f8c26c02237f38d88f9ed7366d
ZiuChen <457353192@qq.com>
2022-10-30 21:46:33 +0800

    update 62(ts) 优化空间复杂度

--- 62.不同路径.ts
+++ 62.不同路径.ts
@@ -6,22 +6,20 @@
 
 // @lc code=start
 function uniquePaths(m: number, n: number): number {
-  const dp: number[][] = []
+  const dp: number[] = []
 
-  // 初始化dp[i][0] = dp[0][j] = 1
-  for (let i = 0; i < m; i++) dp.push(new Array(n).fill(0))
-  for (let i = 0; i < m; i++) dp[i][0] = 1
-  for (let j = 0; j < n; j++) dp[0][j] = 1
+  // 优化空间复杂度: 用滚动的一维数组 保存到达当前行不同列的总路径数
+  // 初始化一维数组 模拟第一行(dp[i][0])全为1
+  for (let i = 0; i < n; i++) dp[i] = 1
 
-  // 按照递推公式与遍历顺序 填入dp
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+  for (let j = 1; j < m; j++) {
+    for (let i = 1; i < n; i++) {
+      dp[i] += dp[i - 1]
     }
   }
 
-  // 返回右下角的dp值 即为结果
-  return dp[m - 1][n - 1]
+  // 返回当前一维数组最末一个元素 即为到达右下角的路径数
+  return dp[n - 1]
 }
 // @lc code=end
 
