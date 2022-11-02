13ae3a32348d62ca79c3f01f03f4cb61b36a7855
ZiuChen <457353192@qq.com>
2022-11-02 12:31:36 +0800

    add 77(ts) 2022/11/2

---
+++ 77.组合.ts
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=77 lang=typescript
+ *
+ * [77] 组合
+ */
+
+// @lc code=start
+function combine(n: number, k: number): number[][] {
+  // 定义全局变量 存储结果
+  const res: number[][] = []
+  const combine: number[] = []
+
+  /**
+   * 回溯函数
+   * @param n 树的宽度
+   * @param k 树的深度
+   * @param startIndex 下一次递归时的开始索引 因为需要无重复选取
+   */
+  const backTracing = (n: number, k: number, startIndex: number) => {
+    // 回溯中止条件: 当前组合已满足个数要求k
+    if (combine.length === k) {
+      res.push([...combine]) // 解构深拷贝
+      return
+    }
+    // 从上次遍历的结果开始 遍历至目标值n
+    for (let i = startIndex; i <= n; i++) {
+      // 将值加入到当前组合中
+      combine.push(i)
+      // 递归调用回溯函数
+      backTracing(n, k, i + 1)
+      // 递归返回 撤销当前处理的节点 回到上一层
+      combine.pop()
+    }
+  }
+  // 开始回溯
+  backTracing(n, k, 1)
+
+  return res
+}
+// @lc code=end
+
+console.log(combine(4, 2))
