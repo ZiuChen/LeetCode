ece8be804bbfa07554e1b2fc41f97e35eb1ddd19
ZiuChen <457353192@qq.com>
2022-11-05 16:58:06 +0800

    update: 40(ts) 优化算法 用stratIndex去重

--- 40.组合总和-ii.ts
+++ 40.组合总和-ii.ts
@@ -8,17 +8,10 @@
 function combinationSum2(candidates: number[], target: number): number[][] {
   const res: number[][] = []
   const combine: number[] = []
-  const used: boolean[] = new Array(candidates.length).fill(false)
 
   // 与39.组合总数不同的是 同一个位置的数不允许被重复选取
-  // 故需要维护一个used数组 记录哪些位置的数字被使用过了
-  const backTracking = (
-    candidates: number[],
-    target: number,
-    sum: number,
-    startIndex: number,
-    used: boolean[]
-  ) => {
+  // 使用startIndex去重也可以
+  const backTracking = (candidates: number[], target: number, sum: number, startIndex: number) => {
     if (sum > target) return
     // 满足条件 保存结果 函数返回
     if (sum === target) {
@@ -26,24 +19,20 @@ function combinationSum2(candidates: number[], target: number): number[][] {
       return
     }
 
+    // 此处sum + candidates[i] <= target为剪枝操作
     for (let i = startIndex; i < candidates.length && sum + candidates[i] <= target; i++) {
       // 同一树层 使用过的元素则跳过
-      // 当前取的candidate[i] 是由candidate[i-1]回溯而来 如果二者相同则跳过
-      // used[i-1] == true: 同一树枝 candidate[i]使用过
-      // used[i-1] == false: 同一树层 candidate[i]使用过
-      if (i > startIndex && candidates[i] === candidates[i - 1] && used[i - 1] === false) {
+      if (i > startIndex && candidates[i] === candidates[i - 1]) {
         continue
       }
-      // 更新当前状态到sum与combine与used
+      // 更新当前状态到sum与combine
       sum += candidates[i]
       combine.push(candidates[i])
-      used[i] = true
       // 递归调用回溯函数
       // 注意: 与39.组合总数的区别 此处传入的是i+1而不是i
       // 因为每个数字在每个组合中只能使用一次
-      backTracking(candidates, target, sum, i + 1, used)
+      backTracking(candidates, target, sum, i + 1)
       // 回溯
-      used[i] = false
       sum -= candidates[i]
       combine.pop()
     }
@@ -52,7 +41,7 @@ function combinationSum2(candidates: number[], target: number): number[][] {
   // 必要: 给candidates排序 让相同值的元素排在一起
   candidates.sort((a, b) => a - b)
 
-  backTracking(candidates, target, 0, 0, used)
+  backTracking(candidates, target, 0, 0)
 
   return res
 }
