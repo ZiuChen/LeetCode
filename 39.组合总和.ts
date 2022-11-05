85631cfb360bb0c7a1b0fef42df70a8bb49de400
ZiuChen <457353192@qq.com>
2022-11-05 16:11:23 +0800

    chore: 39(ts) 注释更新

--- 39.组合总和.ts
+++ 39.组合总和.ts
@@ -29,6 +29,7 @@ function combinationSum(candidates: number[], target: number): number[][] {
       if (sumVal + candidates[i] <= target) {
         // 当前值相加后 仍未得到目标结果 将当前数更新到组合中
         combinate.push(candidates[i])
+        // 重点: 传入递归的backTracing函数的 i不必+1了 表示可以重复选取当前值加入到组合中
         backTracking(combinate, sumVal + candidates[i], i) // 继续递归调用 直至找到目标结果返回
         combinate.pop() // 找到目标结果 递归调用返回时 将组合顶部的元素弹出 即: 回溯到上一步
       }
