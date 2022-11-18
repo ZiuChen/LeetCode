77b1c92efa2fbff42b5a9f9156c2a73d4f98c504
ZiuChen <457353192@qq.com>
2022-11-18 18:04:03 +0800

    update: 27(ts) 2022/11/18

--- 27.移除元素.ts
+++ 27.移除元素.ts
@@ -6,16 +6,20 @@
 
 // @lc code=start
 function removeElement(nums: number[], val: number): number {
-  if (nums.length === 0) return 0
-  if (nums.indexOf(val) === -1) return nums.length
-  let len = nums.length
-  for (let i = 0; i < nums.length; i++) {
-    if (val === nums[i]) {
-      nums.splice(i, 1)
-      i--
-      len--
+  if (!nums.length) return 0
+  if (!nums.includes(val)) return nums.length
+
+  // 定义快指针与慢指针
+  // 快指针 -> 寻找新数组的下一个元素
+  // 慢指针 -> 指向新数组的栈顶
+  let slowIndex = 0
+
+  for (let fastIndex = 0; fastIndex < nums.length; fastIndex++) {
+    if (val !== nums[fastIndex]) {
+      nums[slowIndex++] = nums[fastIndex]
     }
   }
-  return len
+
+  return slowIndex
 }
 // @lc code=end
